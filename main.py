a57813489953af845118a4a629f68f909ab6c822
Andres Lucian Laptes Costan <alaptes.210404@gmail.com>
2025-05-03 11:33:31 +0200

    Push

--- main.py
+++ main.py
@@ -19,6 +19,13 @@ def get_Path(filename, tipo) -> str:
 
 #Transformo data float  
 def ProcesamientoInicial(data):
+    data.columns = [
+            unidecode(col.strip().lower()
+            .replace(" ", "_")
+            .replace("(", "")
+            .replace(")", "")
+            .replace("º", "n")) for col in data.columns]
+    
     df = data.with_columns([
         pl.col("importe_orig.").cast(pl.Float64, strict=False),
         pl.col("importe_pagado").cast(pl.Float64, strict=False),
@@ -29,17 +36,35 @@ def ProcesamientoInicial(data):
     return df
 
 
+def limpiarDataSet(data):
+    columnas = data.columns
+
+    #Eliminamos null
+    df = data.drop_nulls(columnas)
+
+    #Horas correctas
+    df = df.filter((pl.col("fin_transaccion_utc") >= pl.col("hora_inicio_utc")))
+    df = df.filter(pl.col("fecha_envio_utc") >= pl.col("fin_transaccion_utc"))
+
+    #Id transaccion unica
+    df = df.unique(subset=["id_transaccion"])
+
+    #Importes Negativos
+    #Ingreso y gasto simultaneo
+    
+
+    print (df)
+
 def Categorias(data):
     #Caracterizacion
-
     dinero = data.select([pl.col("id_transaccion"), pl.col("ingreso"), pl.col("gasto"), pl.col("id_gasto")])
     tipoTransaccion = data.select([pl.col("id_transaccion"), pl.col("tipo"), pl.col("descripcion"), pl.col("descripcion_gasto")])
     categoria = data.select([pl.col("id_transaccion"), pl.col("categoria"), pl.col("codigo_categoria")])
     mismaDivisa = data.select([pl.col("id_transaccion"), (pl.col("divisa_original") == pl.col("moneda_de_pago")).alias("misma_divisa")])
     infoPagador = data.select([pl.col("id_transaccion"), pl.col("nn_tarjeta"), pl.col("pagador")])
-    frecuenciaTemporal = (data.with_columns([pl.col("hora_inicio_utc").dt.date().alias("dia")]).group_by("dia").agg([pl.count().alias("n_transacciones")]).sort("dia"),
-                          data.with_columns([pl.col("hora_inicio_utc").dt.strftime("%Y-%m").alias("mes")]).group_by("mes").agg([pl.count().alias("n_transacciones")]).sort("mes"),
-                          data.with_columns([pl.col("hora_inicio_utc").dt.strftime("%Y").alias("año")]).group_by("año").agg([pl.count().alias("n_transacciones")]).sort("año")
+    frecuenciaTemporal = (data.with_columns([pl.col("hora_inicio_utc").dt.date().alias("dia")]).group_by("dia").agg([pl.len().alias("n_transacciones")]).sort("dia"),
+                          data.with_columns([pl.col("hora_inicio_utc").dt.strftime("%Y-%m").alias("mes")]).group_by("mes").agg([pl.len().alias("n_transacciones")]).sort("mes"),
+                          data.with_columns([pl.col("hora_inicio_utc").dt.strftime("%Y").alias("año")]).group_by("año").agg([pl.len().alias("n_transacciones")]).sort("año")
                         )
 
     return (dinero, tipoTransaccion, categoria, mismaDivisa, infoPagador, frecuenciaTemporal)
@@ -55,12 +80,18 @@ def guardarEnCSV(path, tuple, nombreArchivo):
     mismaDivisa.write_csv(f"{path}/{nombreArchivo}misma_divisa.csv")
     infoPagador.write_csv(f"{path}/{nombreArchivo}info_pagador.csv")
 
-    frecuenciaTemporal[0].write_csv(f"{path}/{nombreArchivo}frecuencia_por_dia.csv")
-    frecuenciaTemporal[1].write_csv(f"{path}/{nombreArchivo}frecuencia_por_mes.csv")
-    frecuenciaTemporal[2].write_csv(f"{path}/{nombreArchivo}frecuencia_por_year.csv")
+    frecuenciaTemporal[0].write_csv(f"{path}/{nombreArchivo}frecuencia_dia.csv")
+    frecuenciaTemporal[1].write_csv(f"{path}/{nombreArchivo}frecuencia_mes.csv")
+    frecuenciaTemporal[2].write_csv(f"{path}/{nombreArchivo}frecuencia_year.csv")
+
+def exportarArchivos(tuple):
+    current_dir = Path(__file__).parent
+    csv_dir = current_dir / 'DataSets'
+    guardarEnCSV(csv_dir, tuple, "E1")
 
 def crearNuevosDataSets(base):
     tipoCorrecto = ProcesamientoInicial(base)
+    limpiarDataSet(tipoCorrecto)
     NuevaData = Categorias(tipoCorrecto)    
     return NuevaData
 
@@ -69,15 +100,10 @@ if __name__ == "__main__":
     try:
         path = get_Path("E1", "csv")
         data = pl.read_csv(path, try_parse_dates=True)
-        data.columns = [
-            unidecode(col.strip().lower()
-            .replace(" ", "_")
-            .replace("(", "")
-            .replace(")", "")
-            .replace("º", "n")) for col in data.columns]        
+              
         final = crearNuevosDataSets(data)
-        current_dir = Path(__file__).parent
-        csv_dir = current_dir / 'DataSets'
-        guardarEnCSV(csv_dir, final, "E1")
+        
+        #exportarArchivos(final)
+        
     except Exception as e:
         print(f"Error: {str(e)}")
