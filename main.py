f8dc58be12b42ea8b010a91a3174c181968d6902
Andres Lucian Laptes Costan <alaptes.210404@gmail.com>
2025-05-03 12:49:50 +0200

    push

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import polars as pl
 import datetime as dt
 import os
+import numpy as np
 from pathlib import Path
 from unidecode import unidecode
+from sklearn.ensemble import IsolationForest
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import re
 
 def get_Path(filename, tipo) -> str:
     if not filename.endswith(tipo):
@@ -41,7 +47,6 @@ def ProcesamientoInicial(data):
         pl.col("imp._impuestos").cast(pl.Float64, strict=False)
     ])
     return df
-
 
 def limpiarDataSet(data):
     columnas = data.columns
@@ -103,9 +108,37 @@ def exportarArchivos(tuple):
 def crearNuevosDataSets(base):
     tipoCorrecto = ProcesamientoInicial(base)
     setLimpaido = limpiarDataSet(tipoCorrecto)
-    NuevaData = Categorias(setLimpaido)    
+    #NuevaData = Categorias(setLimpaido)    
     
-    return NuevaData
+    
+    #esto tiene que devolver NuevaData despues de los analisis 
+    return setLimpaido
+
+def IsolationAnalisis(data):
+    columnas = ["hora_inicio_utc", "fin_transaccion_utc", "importe_orig.", "importe_pagado", "comision", "tasa_impuestos", "imp._impuestos", "ingreso", "gasto"]
+
+    X = data.select(columnas).to_numpy()
+
+    # Normalizar los datos
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    isolation_forest = IsolationForest(
+        n_estimators=100,
+        contamination=0.1,  # Esperamos aproximadamente un 10% de anomalías
+        random_state=42)
+    isolation_forest.fit(X_scaled)
+
+    # Predecir anomalías (-1 para anomalías, 1 para observaciones normales)
+    predictions = isolation_forest.predict(X_scaled)
+    anomaly_scores = isolation_forest.decision_function(X_scaled)
+
+    df = data.with_columns([
+        pl.Series(name="anomalia_numerica", values=(predictions == -1).astype(int)),
+        pl.Series(name="score_anomalia", values=anomaly_scores)
+    ])
+
+    return df
 
 if __name__ == "__main__":
     
@@ -114,6 +147,8 @@ if __name__ == "__main__":
         data = pl.read_csv(path, try_parse_dates=True)
               
         final = crearNuevosDataSets(data)
+        info = IsolationAnalisis(final)
+        print(info.filter(pl.col("anomalia_numerica") == 1))
         #exportarArchivos(final)
     except Exception as e:
         print(f"Error: {str(e)}")
